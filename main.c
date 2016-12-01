16db9d0cc60ef185be3d50460e5172bfb66749ce
agerome <alex.gerome@utexas.edu>
2016-12-01 03:37:03 -0600

    Implemented a naive P controller. Added skeleton for I and D. Now we will need to tweak with constants.

--- main.c
+++ main.c
@@ -11,6 +11,7 @@
 #include "motor.h"
 
 accel_t accel_frame;
+accel_t prev_accel_frame;
 
 void heartbeat_init(void) {
     SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
@@ -32,7 +33,11 @@ main(void)
     motor_init(M1);
     printf("Init complete, starting up the quadcopter!\n\n");
 
+    get_acceleration(accel_frame);
     set_motor(M1, 500);
+
+    int16_t Px, Py, Pz, Ix, Iy, Iz, Dx, Dy, Dz;
+    int16_t M1_speed, M2_speed, M3_speed, M4_speed;
     //
     // Loop forever.
     //
@@ -73,6 +78,69 @@ main(void)
         for(ui32Loop = 0; ui32Loop < 200000; ui32Loop++)
         {
         }
+
+
+        //PID controller
+        //M1 & M4 are opposites and spin Clockwise to give +z acceleration
+        //Gives acceleration in 12 bit number on scale of 0-4095, mapped to -2 to +2 g
+        //For ALL motors, values are calculted in the same way;
+        // BUT motors 2 and 3 are wired in the opposite direction for simplicity in code
+        prev_accel_frame = accel_frame
+        get_acceleration(accel_frame);
+
+        //P -- map the Gs to the motor duty cycle (0-1000)
+        Px = (4095-accel_frame.x)*1000/4095;
+        Py = (4095-accel_frame.y)*1000/4095;
+        Pz = (4095-accel_frame.z)*1000/4095;
+
+        //I -- TODO: not yet done
+        Ix = 0; 
+        Iy = 0;
+        Iz = 0;
+
+        //D -- TODO: not yet done
+        Dx = 0; 
+        Dy = 0;
+        Dz = 0;
+
+        //Set new motor speeds: M1, M3 left side; M1, M2 top.
+        // +x axis is right, +y is bottom of the board, +z is up into the air
+        // Δx -> left (-)/right (+)
+        // Δy -> top (-)/bottom (+)
+        // Δz -> down (-)/up (+)
+        M1_speed = (0 -Px -Py +Pz)/3;
+        M2_speed = (   Px -Py +Pz)/3;
+        M3_speed = (0 -Px +Py +Pz)/3;
+        M4_speed = (   Px +Py +Pz)/3;
+
+        if(M1_speed < 0){
+            M1_speed = 0;
+        }else if(M1_speed > 1000){
+            M1_speed = 1000;
+        }
+
+        if(M2_speed < 0){
+            M2_speed = 0;
+        }else if(M2_speed > 1000){
+            M2_speed = 1000;
+        }
+
+        if(M3_speed < 0){
+            M3_speed = 0;
+        }else if(M3_speed > 1000){
+            M3_speed = 1000;
+        }
+
+        if(M4_speed < 0){
+            M4_speed = 0;
+        }else if(M4_speed > 1000){
+            M4_speed = 1000;
+        }
+
+        set_motor(M1, M1_speed);
+        set_motor(M2, M2_speed);
+        set_motor(M3, M3_speed);
+        set_motor(M4, M4_speed);
 
     }
 }
